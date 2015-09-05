12906a6afb27393d4274bd88fd25ff0e91cc7f55
ed1rac <edkallenn@gmail.com>
2015-09-04 23:52:30 -0500

    Primeiros Programas

---
+++ Aula01/exemploPratico01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <math.h>    // necessário para sin()
+
+int main()
+{
+  float y;  /*Declarando a variável y   */
+  y = sin(1.5);
+  printf("Programa Besta do SENO\n");
+  printf("y = %f\n\n",y);
+  printf("");   //comentários de uma linha sao permitidos
+  system("pause");
+  return 0;
+}
+/*  O comentário pode conter mais de uma linha
+    como neste exemplo. Comentários não tem ação
+    ou não geram nenhuma ação por parte do compu-
+    tador. Eles são ignorados pelo compilador.
+    Servem para melhorar a legibilidade de programas
+*/
