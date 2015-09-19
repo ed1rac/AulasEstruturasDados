036f8a71fd86ca689ddf268d22eb0a0b7e6768b6
ed1rac <edkallenn@gmail.com>
2015-09-18 21:39:46 -0500

    Comando Switch e Função fatorial

---
+++ Funcoes/fatorialNaoRecursivo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*  Função : Funcao fatorial nao-recursiva
+    Autor : Edkallenn
+    Data : 06/04/2012
+    Observações:            */
+double fat(int n);  //prototipo da funcao
+main(){
+    int i,num;
+    do{         //laco enquanto nao for positivo nao continua
+        printf("\nDigite um numero inteiro positivo:");
+        scanf("%d", &num);
+    }while(num<=0);
+    for (i=1;i<=num;i++){   //modtra TODOS os fatoriais de 1 ate num
+        printf("\nFatorial de %d eh: %.0f", i, fat(i));
+    }
+    getch();
+}
+double fat(int n){
+    int cont;
+    double fatorial;
+    fatorial = 1;
+    for(cont=1;cont<=n;cont++){
+        fatorial*=cont;
+    }
+    return fatorial;
+}
