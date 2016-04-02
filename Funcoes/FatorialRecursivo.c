f825ed5e54fcbbbeb056db7237cc873354d0c59c
ed1rac <edkallenn@gmail.com>
2016-04-02 17:41:16 -0500

    Novas Funções

---
+++ Funcoes/FatorialRecursivo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*  Fun��o : Funcao fatorial recursiva
+    Autor : Edkallenn
+    Data : 06/04/2012
+    Observa��es:
+*/
+
+float fat(int n);
+
+main(){
+    int i,num;
+    do{
+        printf("\nDigite um numero inteiro positivo:");
+        scanf("%d", &num);
+    }while(num<=0);
+
+    for (i=1;i<=num;i++){
+        printf("\nFatorial de %d eh: %.0f", i, fat(i));
+
+    }
+    getch();
+
+
+}
+
+float fat(int n){
+
+    if (n==0)
+        return 1;
+    else
+        return n*fat(n-1);
+
+}
