1c87ea4459a68cb9c7ff23b5773c705a11cd9a16
ed1rac <edkallenn@gmail.com>
2016-04-14 20:34:40 -0500

    Primeiros programas de Arrays

---
+++ VetoresMatrizes/declara_preencheFirstProgram.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*  Função : Segundo contato com arrays (vetores)
+    Autor : Edkallenn
+    Data : 06/04/2012
+    Observações: arquivo-> declaracao_preenchimento.cpp
+*/
+#define MAX 50  //tamanho maximo do vetor
+
+int main(){
+    int x[MAX];
+    int t;
+
+    // Preenche o vetor
+    for (t=0;t<MAX;++t)
+        x[t]=t*2+2; //forma normal - impares
+
+    //Exibe
+    for (t=0;t<MAX;t++)
+        printf("%-3d\t", x[t]);
+
+    printf("\n");
+    //getchar();
+    system("read");
+}
