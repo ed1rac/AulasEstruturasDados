abd3cdac218922439b719b669239f3ccd06cb606
ed1rac <edkallenn@gmail.com>
2017-03-17 19:52:59 -0500

    Programas renomeados

---
+++ 2017-1/Aula01/exemplo01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	printf("Primeiro programa em C!\n");
+	printf("Esse nao e' um Ola mundo!\n");
+    printf("Nao tenho medo de lendas!\n");
+	//getch();
+	system("pause"); //pode ser system("pause") ou getch(); no windows
+	return 0;
+}
