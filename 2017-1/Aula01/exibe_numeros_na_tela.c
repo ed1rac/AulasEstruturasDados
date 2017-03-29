caf6dfa55d63dbcf322908e21aad49b942f69fd3
ed1rac <edkallenn@gmail.com>
2017-03-28 21:28:24 -0500

    primeiros programas com loops

---
+++ 2017-1/Aula01/exibe_numeros_na_tela.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int contador;
+    contador = 1;
+    while(contador<=20){
+        printf("%d\n", contador);
+        contador++;
+    }
+    system("pause");
+}
