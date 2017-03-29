b9f8d32f8e4db0c3ded9f7ae73348c427812c7b5
ed1rac <edkallenn@gmail.com>
2017-03-28 21:39:59 -0500

    mais programas com loops

--- 2017-1/Aula01/exibe_numeros_na_tela.c
+++ 2017-1/Aula01/exibe_numeros_na_tela.c
@@ -2,9 +2,11 @@
 #include<stdlib.h>
 
 int main(){
-    int contador;
+    int contador, numero;
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
     contador = 1;
-    while(contador<=20){
+    while(contador<=numero){
         printf("%d\n", contador);
         contador++;
     }
