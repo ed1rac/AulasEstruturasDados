84cffdf066612040960f7468114eea9f515a8370
ed1rac <edkallenn@gmail.com>
2017-03-28 20:23:09 -0500

    alterações nos programas da segunda aula de condicionais

--- 2017-1/Aula01/par_impar.c
+++ 2017-1/Aula01/par_impar.c
@@ -2,15 +2,21 @@
 #include<stdlib.h>
 
 int main(){
-    //declarar as variaveis
     int num;
-    //ENTRADA
     printf("Digite um numero: ");
     scanf("%d", &num);
-    if(num%2==0){
-        printf("O numero %d e' par!\n", num);
+    if(num>0){
+        if(num<1000){
+            if(num%2==0){
+                printf("O numero %d e' par!\n", num);
+            }else{
+                printf("O numero %d e' impar!\n", num);
+            }  
+        }else{
+            printf("O numero e' maior que 1000!\n");
+        }
     }else{
-        printf("O numero %d e' impar!\n", num);
-    }    
+        printf("O numero e' menor que zero!\n\n'");
+    } 
     system("pause");
 }
