8eb82879dc1f45114953ae05853bb007cd4021f9
ed1rac <edkallenn@gmail.com>
2024-03-19 00:41:48 -0300

    Inclusao de novas pastas

--- UNP/ref/Python/TADs e Classes/Fila.py
+++ UNP/ref/Python/TADs e Classes/Fila.py
@@ -1,35 +1,37 @@
+# Definição do TAD Fila
 class Fila(object):
-  'uma classe de fila clássica'
+    'uma classe de fila clássica'
 
-  def __init__(self):
-    'instancia uma lista vazia'
-    self.items = []
+    def __init__(self):
+        'instancia uma lista vazia'
+        self.items = []
 
-  def esta_vazia(self):
-    'retorna True se a lista está vazia, False caso contrário'
-    return (len(self.items)==0) #se o tamanho for zero
+    def esta_vazia(self):
+        'retorna True se a lista está vazia, False caso contrário'
+        return (len(self.items) == 0)  # se o tamanho for zero
 
-  def enfileira(self, item):    #enqueue
-    'insere item no final da fila'
-    return self.items.append(item)
+    def enfileira(self, item):  # enqueue
+        'insere item no final da fila'
+        return self.items.append(item)
 
-  def desenfileira(self):       #dequeue
-    'remove e retorna o item na frente da fila'
-    if not (self.esta_vazia()):
-      return self.items.pop(0)
-    print('Impossível desenfileirar. Fila está vazia')
-  
-  def imprime(self):
-    'imprime a fila'
-    print(self.items)
+    def desenfileira(self):  # dequeue
+        'remove e retorna o item na frente da fila'
+        if not (self.esta_vazia()):
+            return self.items.pop(0)
+        print('Impossível desenfileirar. Fila está vazia')
 
-  def size(self):
-    'retorna o número de itens na fila'
-    return len(self.items)
+    def imprime(self):
+        'imprime a fila'
+        print(self.items)
+
+    def size(self):
+        'retorna o número de itens na fila'
+        return len(self.items)
 
 
 fila = Fila()
-if fila.esta_vazia: print('Fila está vazia!')
+if fila.esta_vazia:
+    print('Fila está vazia!')
 x = fila.desenfileira()
 fila.enfileira('Edkallenn')
 fila.enfileira('Vanessa')
